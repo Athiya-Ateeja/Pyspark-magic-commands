f2dd78df110535f2f818b4435b6f77389e2c5357
Athiya <118296173+Athiya-Ateeja@users.noreply.github.com>
2023-11-13 23:25:14 +0530

    Add files via upload

---
+++ Magic commands.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Notebook Introduction
+# MAGIC ##UI Introduction
+# MAGIC ##Magic Commands
+# MAGIC - %python
+# MAGIC - %scala
+# MAGIC - %sql
+# MAGIC - %md
+
+# COMMAND ----------
+
+message = " hi welcome to pyspark lecture"
+
+# COMMAND ----------
+
+print(message)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "hello"
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC var = "hi"
+# MAGIC print(var)
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls
+
+# COMMAND ----------
+
+# MAGIC %sh
+# MAGIC ps
